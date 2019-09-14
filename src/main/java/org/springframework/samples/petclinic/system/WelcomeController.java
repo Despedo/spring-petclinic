af56c3237b1aa1d2826cb829c35f98bbef2661a4
Oleksandr Vechirko <oleksandr_vechirko@epam.com>
2019-09-15 00:28:18 +0300

    test

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -17,14 +17,21 @@
 package org.springframework.samples.petclinic.system;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class WelcomeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
     @GetMapping("/")
     public String welcome() {
+        logger.warn("Welcome");
+        logger.info("Welcome");
+        logger.error("Welcome");
         return "welcome";
     }
 }
